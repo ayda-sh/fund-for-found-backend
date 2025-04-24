5dd31a1d8c4eb19fa392801922dc9bb928841936
ayda <aydashamma4@gmail.com>
2025-04-24 15:53:51 +0330

    plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,6 +6,7 @@ export default {
         jwt: {
           expiresIn: '30d',
         },
+        jwtSecret: 'EtiI8cdmt1oW+bMFu8pTHn/MO3TuMj2fOtK1KfgV0pEsCle++sZD4cmQpXL7I8OHFH0UZN8NgsSUAqt8KWk/EQ==', 
       },
     },
   };
